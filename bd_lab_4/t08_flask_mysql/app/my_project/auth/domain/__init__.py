52f9cdc837993d98050cd6cc32b7c7e0a5430d31
NastiaKed <stasaked@gmail.com>
2023-11-20 16:45:11 +0200

    lab_5
    added lab5

---
+++ bd_lab_4/t08_flask_mysql/app/my_project/auth/domain/__init__.py
@@ -0,0 +1,10 @@
+"""
+2022
+apavelchak@gmail.com
+© Andrii Pavelchak
+"""
+
+# Import here Domain Class that are needed for ORM
+# orders DB
+from t08_flask_mysql.app.my_project.auth.domain.orders.client import Client
+from t08_flask_mysql.app.my_project.auth.domain.orders.client_type import ClientType
